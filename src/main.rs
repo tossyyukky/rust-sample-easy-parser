80a1f6d23267a9e36948c4dcf74dd123eb9bd76c
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-05-03 00:02:29 +0900

    add: helper method for token

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ enum TokenKind {
     /// +
     Plus,
     /// -
-    Minux,
+    Minus,
     /// *
     Asterisk,
     /// /
@@ -50,3 +50,34 @@ enum TokenKind {
 
 // TokenKindにアノテーションをつけたものをTokenとして定義しておく
 type Token = Annot<TokenKind>;
+
+// トークン生成用ヘルパーメソッド
+impl Token {
+    fn number(n: u64, loc: Loc) -> Self {
+        Self::new(TokenKind::Number(n), loc)
+    }
+
+    fn plus(loc: Loc) -> Self {
+        Self::new(TokenKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(TokenKind::Minus, loc)
+    }
+
+    fn asterisk(loc: Loc) -> Self {
+        Self::new(TokenKind::Asterisk, loc)
+    }
+
+    fn slash(loc: Loc) -> Self {
+        Self::new(TokenKind::Slash, loc)
+    }
+
+    fn lparen(loc: Loc) -> Self {
+        Self::new(TokenKind::LParen, loc)
+    }
+
+    fn rparen(loc: Loc) -> Self {
+        Self::new(TokenKind::RParen, loc)
+    }
+}
